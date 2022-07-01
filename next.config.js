99b01223853b53cd729e87ec70b12c036e71df57
Raju Khattri <rajukhattri@proton.me>
2022-07-01 19:08:20 +0530

    Update image domains

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['scontent-iad3-1.cdninstagram.com'],
+    domains: [
+      'scontent-iad3-1.cdninstagram.com',
+      'instagram.fvns4-1.fna.fbcdn.net',
+    ],
   },
 }
